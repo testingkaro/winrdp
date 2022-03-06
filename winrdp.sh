8e19ee6531f470c344f7424a5fd29a5fa023cff5
testingkaro <100287680+testingkaro@users.noreply.github.com>
2022-03-06 21:32:25 +0530

    Update winrdp.sh

--- winrdp.sh
+++ winrdp.sh
@@ -1,44 +1,23 @@
-#!/bin/bash
-FROM ubuntu:18.04
-RUN apt-get update -y
-RUN apt-get install -y qemu-kvm libvirt-daemon-system libvirt-dev
-RUN apt-get install -y linux-image-$(uname -r)
-RUN apt-get install -y curl net-tools jq
-RUN apt-get autoclean
-RUN apt-get autoremove
-RUN curl -O https://releases.hashicorp.com/vagrant/$(curl -s https://checkpoint-api.hashicorp.com/v1/check/vagrant  | jq -r -M '.current_version')/vagrant_$(curl -s https://checkpoint-api.hashicorp.com/v1/check/vagrant  | jq -r -M '.current_version')_x86_64.deb
-RUN dpkg -i vagrant_$(curl -s https://checkpoint-api.hashicorp.com/v1/check/vagrant  | jq -r -M '.current_version')_x86_64.deb
-RUN vagrant plugin install vagrant-libvirt
-RUN vagrant box add --provider libvirt peru/windows-10-enterprise-x64-eval
-RUN vagrant init peru/windows-10-enterprise-x64-eval
-COPY winrdp.sh /
-ENTRYPOINT ["/winrdp.sh"]
-#!/bin/bash
-set -eou pipefail
-
-chown root:kvm /dev/kvm
-service libvirtd start
-service virtlogd start
-VAGRANT_DEFAULT_PROVIDER=libvirt vagrant up
-iptables-save > $HOME/firewall.txt
-iptables -X
-iptables -t nat -F
-iptables -t nat -X
-iptables -t mangle -F
-iptables -t mangle -X
-iptables -P INPUT ACCEPT
-iptables -P FORWARD ACCEPT
-iptables -P OUTPUT ACCEPT
-
-iptables -A FORWARD -i eth0 -o virbr1 -p tcp --syn --dport 3389 -m conntrack --ctstate NEW -j ACCEPT
-iptables -A FORWARD -i eth0 -o virbr1 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-iptables -A FORWARD -i virbr1 -o eth0 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 3389 -j DNAT --to-destination 192.168.121.68
-iptables -t nat -A POSTROUTING -o virbr1 -p tcp --dport 3389 -d 192.168.121.68 -j SNAT --to-source 192.168.121.1
-
-iptables -D FORWARD -o virbr1 -j REJECT --reject-with icmp-port-unreachable
-iptables -D FORWARD -i virbr1 -j REJECT --reject-with icmp-port-unreachable
-iptables -D FORWARD -o virbr0 -j REJECT --reject-with icmp-port-unreachable
-iptables -D FORWARD -i virbr0 -j REJECT --reject-with icmp-port-unreachable
-
-exec "$@"
+rm -rf ngrok  ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+echo "Download ngrok"
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+read -p "Ctrl + V Authtoken: " CRP 
+./ngrok authtoken $CRP 
+nohup ./ngrok tcp 3389 &>/dev/null &
+./ngrok tcp 3389 &>/dev/null &
+echo Downloading File From akuh.net
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A6DCF7707EBC211F > /dev/null 2>&1
+sudo apt-add-repository "deb http://ppa.launchpad.net/ubuntu-mozilla-security/ppa/ubuntu bionic main" > /dev/null 2>&1
+sudo apt update > /dev/null 2>&1
+sudo apt-get install firefox -y > /dev/null 2>&1
+sudo apt install xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils > /dev/null 2>&1
+sudo apt install xrdp -y > /dev/null 2>&1
+sudo systemctl status xrdp > /dev/null 2>&1
+sudo adduser xrdp ssl-cert > /dev/null 2>&1
+sudo systemctl restart xrdp > /dev/null 2>&1
+sudo apt-get install -y xfce4-terminal > /dev/null 2>&1
+sudo sed -i.bak '/fi/a xfce4-session \n' /etc/xrdp/startwm.sh
+sudo service xrdp start
+clear
+sudo ufw allow 3389
